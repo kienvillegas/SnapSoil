75e956277a8af61a4a81320ba86697c34465c87f
Kien Villegas <villegaskien@gmail.com>
2025-02-13 22:10:36 +0800

    updated the project

---
+++ app/src/main/java/com/example/snapsoil/Info.java
@@ -0,0 +1,30 @@
+package com.example.snapsoil;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Info extends Fragment {
+
+
+    public Info() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+       return inflater.inflate(R.layout.fragment_info, container, false);
+
+    }
+}
